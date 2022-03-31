e330a86d89d1d7352f213e9121abdffcb828b907
Guy Itzhaki <Guy.Itzhaki@Honeywell.com>
2022-03-31 09:55:51 +0300

    from home

--- src/main/scala/demo/Demo.scala
+++ src/main/scala/demo/Demo.scala
@@ -3,7 +3,15 @@ package demo
 object Demo {
   def main(args: Array[String]): Unit = {
     println("Hi there")
-    println("Eliran was here")
-
+    var a = 12.asInstanceOf[Byte]
+    println(a)
+    println("any")
+    var b:Any = 12
+    println(b)
+    var con1 = true
+    var con2 = false
+    println(con1 & con2)
+    //var c = 10.unary_+
+    //println(c)
   }
 }
